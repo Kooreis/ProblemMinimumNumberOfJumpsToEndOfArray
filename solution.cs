60d11a7b91f7b2a705f0524ee12a7e44bfc8ed6d
Kooreis <kooreis.official@gmail.com>
2024-01-06 12:38:25 -0600

    In this update, the main function is created. It reads a line from the console, splits it into an array of strings, and then converts each string to an integer. The resulting array of integers is stored in the variable 'arr'.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,3 @@
-```C#
 using System;
 using System.Linq;
 
@@ -7,34 +6,5 @@ public class Program
     public static void Main()
     {
         int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-        int jumps = MinJumps(arr, arr.Length);
-        Console.WriteLine(jumps);
-    }
-
-    static int MinJumps(int[] arr, int n)
-    {
-        if (n <= 1)
-            return 0;
-        if (arr[0] == 0)
-            return -1;
-        int maxReach = arr[0];
-        int step = arr[0];
-        int jump = 1;
-        for (int i = 1; i < n; i++)
-        {
-            if (i == n - 1)
-                return jump;
-            maxReach = Math.Max(maxReach, i + arr[i]);
-            step--;
-            if (step == 0)
-            {
-                jump++;
-                if (i >= maxReach)
-                    return -1;
-                step = maxReach - i;
-            }
-        }
-        return -1;
     }
 }
-```
