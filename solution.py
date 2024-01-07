c74cd6b7422f013a1c976bb2784052cb5b784d06
Kooreis <kooreis.official@gmail.com>
2024-01-07 12:38:25 -0600

    This part of the script initializes the rest of the jumps array to infinity. This is done because we want to find the minimum number of jumps, so we start with the maximum possible value.

--- solution.py
+++ solution.py
@@ -1,7 +1,2 @@
-def min_jumps(arr, n):
-    jumps = [0 for i in range(n)]
- 
-    if (n == 0) or (arr[0] == 0):
-        return float('inf')
- 
-    jumps[0] = 0
+for i in range(1, n):
+        jumps[i] = float('inf')
