7515292804d92267357194d2fc066e350280e14c
Kooreis <kooreis.official@gmail.com>
2024-01-07 12:38:25 -0600

    In this update, the function 'MinJumps' is called with the array 'arr' and its length as arguments. The result is stored in the variable 'jumps'.

--- solution.cs
+++ solution.cs
@@ -6,5 +6,6 @@ public class Program
     public static void Main()
     {
         int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int jumps = MinJumps(arr, arr.Length);
     }
 }
